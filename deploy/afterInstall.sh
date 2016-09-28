bde9fafefad8becc2e6efa3446c4bcf0ab00ef79
anand vishwakarma <anand@nisostech.com>
2016-09-28 19:19:05 +0530

    add laravel project

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -3,3 +3,4 @@ composer create-project --prefer-dist laravel/laravel blog
 # cp $PATH_MIGRATED/.env.staging $PATH_MIGRATED/.env
 # chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
 # sudo composer install -d $PATH_MIGRATED
+# su -c "composer create-project --prefer-dist laravel/laravel blog" -s /bin/bash devuser
