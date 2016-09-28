f685bd32316f181abbe4a7097e3e89ea28b6dc9d
anand vishwakarma <anand@nisostech.com>
2016-09-28 20:34:22 +0530

    change permission mode

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -4,3 +4,4 @@
 # chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
 # sudo composer install -d $PATH_MIGRATED
 su - devuser -c "composer create-project --prefer-dist laravel/laravel /home/devuser/projects/blog"
+su - devuser -c "chmod -R 777 /home/devuser/projects/blog/bootstrap/cache /home/devuser/projects/blog/storage/logs"
