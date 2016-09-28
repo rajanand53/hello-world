b62d983c60429713e8c1b4435e9c242c8c499dee
anand vishwakarma <anand@nisostech.com>
2016-09-28 19:35:27 +0530

    add laravel changes

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,6 +1,6 @@
-composer create-project --prefer-dist laravel/laravel blog
+#composer create-project --prefer-dist laravel/laravel blog
 # PATH_MIGRATED=/home/devuser/projects/aerod-backend-migrated
 # cp $PATH_MIGRATED/.env.staging $PATH_MIGRATED/.env
 # chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
 # sudo composer install -d $PATH_MIGRATED
-# su -c "composer create-project --prefer-dist laravel/laravel blog" -s /bin/bash devuser
+su -c "composer create-project --prefer-dist laravel/laravel blog" -s /bin/bash devuser
