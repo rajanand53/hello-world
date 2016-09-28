082aa8dae440674e793db2badc6e21340690d79c
anand vishwakarma <anand@nisostech.com>
2016-09-28 19:06:46 +0530

    add laravel project

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,3 +1,4 @@
+composer create-project --prefer-dist laravel/laravel blog
 # PATH_MIGRATED=/home/devuser/projects/aerod-backend-migrated
 # cp $PATH_MIGRATED/.env.staging $PATH_MIGRATED/.env
 # chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
