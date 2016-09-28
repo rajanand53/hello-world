85c0e5cc9b81654b55dd653d059476d0d94af93d
anand vishwakarma <anand@nisostech.com>
2016-09-28 16:36:07 +0530

    appspec added

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,4 +1,4 @@
-PATH_MIGRATED=/home/devuser/projects/aerod-backend-migrated
-cp $PATH_MIGRATED/.env.staging $PATH_MIGRATED/.env
-chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
-sudo composer install -d $PATH_MIGRATED
+# PATH_MIGRATED=/home/devuser/projects/aerod-backend-migrated
+# cp $PATH_MIGRATED/.env.staging $PATH_MIGRATED/.env
+# chmod -R 777 $PATH_MIGRATED/bootstrap/cache $PATH_MIGRATED/storage/logs
+# sudo composer install -d $PATH_MIGRATED
